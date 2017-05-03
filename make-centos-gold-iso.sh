73529b3951a7dd5650d69ff357d2206ebf4d219f
root <root@ipa.foo.local>
2017-05-03 19:58:40 +0000

    saves .iso in /var/pub

--- make-centos-gold-iso.sh
+++ make-centos-gold-iso.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 WORKING="/var/lib/iso"
-ISO="/tmp/centos-gold-7.3.1611.iso"
+PUB='/var/pub'
+
+ISO="${PUB}/centos-gold-7.3.1611.iso"
 
 yum install -y genisoimage
 
@@ -13,6 +15,7 @@ cp -a /media/cdrom/* ${WORKING}
 cp ks/gold.ks ${WORKING}
 cp isolinux.cfg ${WORKING}/isolinux
 
+mkdir -p ${PUB}
 cd ${WORKING}
 mkisofs -o ${ISO} -b isolinux/isolinux.bin -c isolinux/boot.cat --no-emul-boot --boot-load-size 4 --boot-info-table -J -R -V Gold .
 
